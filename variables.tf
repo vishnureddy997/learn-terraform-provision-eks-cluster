8ff5b7e6550eacbcedab2336035f691b82f2244c
vishnureddy997 <57907899+vishnureddy997@users.noreply.github.com>
2022-07-28 12:12:04 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+default = "Access_key value" 
+ }
+variable "secret_key" {                  
+ default = "Secret_key value" 
+ }
+variable "region" {                   
+ default = "us-east-2" 
+   }
